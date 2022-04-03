93f4d241fd9f8a32543fe128ec5438d22329346e
fahadmujawar <76247909+fahadmujawar@users.noreply.github.com>
2022-04-03 14:24:28 +0530

    Update 1_2.py

--- 1_2.py
+++ 1_2.py
@@ -59,15 +59,15 @@ fig = plt.figure(figsize=(8, 5.25))
 def plot_e_field(data, timestep, label):
 """Plot of E field at a single time step"""
 
-plt.plot(data, color='k', linewidth=1)
-plt.ylabel('E$_x$', fontsize='14')
-plt.xticks(np.arange(0, 199, step=20))
-plt.xlim(0, 199)
-plt.yticks(np.arange(0, 1.2, step=1))
-plt.ylim(-0.2, 1.2)
-plt.text(100, 0.5, 'T = {}'.format(timestep),
-          horizontalalignment='center')
-plt.xlabel('{}'.format(label))
+   plt.plot(data, color='k', linewidth=1)
+   plt.ylabel('E$_x$', fontsize='14')
+   plt.xticks(np.arange(0, 199, step=20))
+   plt.xlim(0, 199)
+   plt.yticks(np.arange(0, 1.2, step=1))
+   plt.ylim(-0.2, 1.2)
+   plt.text(100, 0.5, 'T = {}'.format(timestep),
+              horizontalalignment='center')
+    plt.xlabel('{}'.format(label))
 
 
 # Plot the E field at each of the time steps saved earlier
